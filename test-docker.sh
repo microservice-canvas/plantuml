ec2808cd465275d1d14c6fad2e2201b9bf15c968
Chris Richardson <chris@chrisrichardson.net>
2022-01-11 19:42:32 -0800

    Fixed ARM image, fixed test script

--- test-docker.sh
+++ test-docker.sh
@@ -1,5 +1,7 @@
 #! /bin/bash -e
 
+mkdir -p example-pngs
+
 docker run -i --rm --net=none -e PLANTUML_OPTIONS microservicesio/plantuml:test-build-${CIRCLE_BUILD_NUM:-local} < example-diagrams/example-diagram.txt > example-pngs/example-diagram.png
 
 file example-pngs/example-diagram.png
