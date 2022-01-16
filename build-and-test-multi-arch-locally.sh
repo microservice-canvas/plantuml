982c727c0c6a59f10c1fb649ebb2474e59e4a778
Chris Richardson <chris@chrisrichardson.net>
2022-01-16 08:29:31 -0800

    Changed registry port to avoid conflict

--- build-and-test-multi-arch-locally.sh
+++ build-and-test-multi-arch-locally.sh
@@ -2,11 +2,26 @@
 
 docker-compose up -d
 
-BUILD_TAG=host.docker.internal:5000/plantuml:test-build
-TAG=localhost:5000/plantuml:test-build
+PORT=$(docker-compose port registry 5000 | sed 's/.*://')
+BUILD_TAG=host.docker.internal:$PORT/plantuml:test-build
+TAG=localhost:$PORT/plantuml:test-build
+
+echo BUILD_TAG=$BUILD_TAG TAG=$TAG
 
 docker buildx build --platform linux/amd64,linux/arm64 -t $BUILD_TAG \
   --output=type=image,push=true,registry.insecure=true plantuml-container
+
+echo == Architecture
+
+docker inspect $TAG --format '{{.Architecture}}'
+
+echo == The manifest
+
+docker manifest inspect --insecure $TAG
+
+echo == Test
+
+mkdir -p example-pngs
 
 docker run -i --rm --net=none -e PLANTUML_OPTIONS $TAG < example-diagrams/example-diagram.txt > example-pngs/example-diagram.png
 
