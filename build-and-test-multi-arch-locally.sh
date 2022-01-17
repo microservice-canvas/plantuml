fa1e016513a0ec138352cc320cc327629fb0a918
Chris Richardson <chris@chrisrichardson.net>
2022-01-17 15:48:19 -0800

    wip - experiment

--- build-and-test-multi-arch-locally.sh
+++ build-and-test-multi-arch-locally.sh
@@ -2,17 +2,27 @@
 
 docker-compose up -d
 
+XX=plantuml
+XX=test-tiny
+
 PORT=$(docker-compose port registry 5000 | sed 's/.*://')
-BUILD_TAG=host.docker.internal:$PORT/plantuml:test-build
-TAG=localhost:$PORT/plantuml:test-build
+BUILD_TAG=host.docker.internal:$PORT/plantuml:$XX
+TAG=${LOCAL_DOCKER_HOST-localhost:$PORT/}plantuml:$XX
 
 echo BUILD_TAG=$BUILD_TAG TAG=$TAG
 
 docker buildx build --platform linux/amd64,linux/arm64 -t $BUILD_TAG \
-  --output=type=image,push=true,registry.insecure=true plantuml-container
+  --output=type=image,push=true,registry.insecure=true -f plantuml-container/Dockerfile.tiny .
 
 echo == Architecture
 
+echo TAG=$TAG
+
+docker images
+
+env
+
+docker pull $TAG
 docker inspect $TAG --format '{{.Architecture}}'
 
 echo == The manifest
