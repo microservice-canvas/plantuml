97d605e57235be0656916ed7719bd8c3fb95124e
Chris Richardson <chris@chrisrichardson.net>
2022-01-18 19:37:14 -0800

    Clean up

--- build-and-test-multi-arch-circleci.sh
+++ build-and-test-multi-arch-circleci.sh
@@ -1,30 +1,30 @@
 #! /bin/bash -e
 
-TAG=microservicesio/plantuml:test-build-${CIRCLE_SHA1:-local}
+TARGET_IMAGE=microservicesio/plantuml:test-build-${CIRCLE_SHA1:-local}
 
 docker login -u ${DOCKER_USER_ID?} -p ${DOCKER_PASSWORD?}
 
-docker buildx build --platform linux/amd64,linux/arm64 -t $TAG --push plantuml-container
+docker buildx build --platform linux/amd64,linux/arm64 -t $TARGET_IMAGE --push plantuml-container
 
 echo == Architecture
 
-echo TAG=$TAG
+echo TARGET_IMAGE=$TARGET_IMAGE
 
 docker images
 
 env
 
-docker pull $TAG
-docker inspect $TAG --format '{{.Architecture}}'
+docker pull $TARGET_IMAGE
+docker inspect $TARGET_IMAGE --format '{{.Architecture}}'
 
 echo == The manifest
 
-docker manifest inspect --insecure $TAG
+docker manifest inspect --insecure $TARGET_IMAGE
 
 echo == Test
 
 mkdir -p example-pngs
 
-docker run -i --rm --net=none -e PLANTUML_OPTIONS $TAG < example-diagrams/example-diagram.txt > example-pngs/example-diagram.png
+docker run -i --rm --net=none -e PLANTUML_OPTIONS $TARGET_IMAGE < example-diagrams/example-diagram.txt > example-pngs/example-diagram.png
 
 file example-pngs/example-diagram.png
