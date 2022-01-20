cf21bd0d676ae2b327fa44bbcb5264d6aff90720
Chris Richardson <chris@chrisrichardson.net>
2022-01-20 08:48:19 -0800

    Publish a tested, multi-architecture image with the correct tag

--- deploy-artifacts.sh
+++ deploy-artifacts.sh
@@ -3,18 +3,26 @@
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 if [[  $BRANCH == "master" ]] ; then
-  VERSION=BUILD-${CIRCLE_BUILD_NUM?}
+  TARGET_TAG=BUILD-${CIRCLE_BUILD_NUM?}
 elif [[  $BRANCH =~ RELEASE$ ]] ; then
-  VERSION=$BRANCH
+  TARGET_TAG=$BRANCH
 elif [[  $BRANCH =~ M[0-9]+$ ]] ; then
-  VERSION=$BRANCH
+  TARGET_TAG=$BRANCH
 elif [[  $BRANCH =~ RC[0-9]+$ ]] ; then
-  VERSION=$BRANCH
+  TARGET_TAG=$BRANCH
 else
-  VERSION=${BRANCH}-${CIRCLE_BUILD_NUM?}
+  TARGET_TAG=${BRANCH}-${CIRCLE_BUILD_NUM?}
 fi
 
 docker login -u ${DOCKER_USER_ID?} -p ${DOCKER_PASSWORD?}
 
-docker tag plantuml:latest microservicesio/plantuml:$VERSION
-docker push microservicesio/plantuml:$VERSION
+TARGET_IMAGE=microservicesio/plantuml:$TARGET_TAG
+
+SRC_TAG=test-build-${CIRCLE_SHA1?}
+
+echo Retagging $SRC_TAG $TARGET_IMAGE
+
+SOURCES=$(docker manifest inspect docker.io/microservicesio/plantuml:${SRC_TAG} | \
+jq -r '.manifests[].digest  | sub("^"; "docker.io/microservicesio/plantuml@")')
+
+docker buildx imagetools create -t ${TARGET_IMAGE} $SOURCES
